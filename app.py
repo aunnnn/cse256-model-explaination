e76b8e8479f14814b89d45d0dbb65042c54270b1
Wirawit Rueopas <aun.wirawit@gmail.com>
2019-05-25 17:16:54 -0700

    Remove unused pandas

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-import pandas as pd
-
 from components.example_layout import example_layout
 
 external_stylesheets = [
